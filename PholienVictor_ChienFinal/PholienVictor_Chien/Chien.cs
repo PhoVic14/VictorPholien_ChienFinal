1ab67b6de763eb4871516abc04ae030a6ce80c7e
PhoVic14 <91669693+PhoVic14@users.noreply.github.com>
2023-09-12 22:20:05 +0200

    Ajout du final chien

---
+++ PholienVictor_ChienFinal/PholienVictor_Chien/Chien.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PholienVictor_Chien
+{
+    class Chien
+    {
+        private string _nom;
+        private string _race;
+        private uint _age;
+        private bool _enOrdreDeVaccin;
+        private bool _puce;
+        private string _genre;
+        private bool _sterilise;
+
+        public Chien(string nom, string race, uint age, bool enOrdreDeVaccin, bool puce, string genre, bool sterilise, bool dresse)
+        {
+            _nom = nom;
+            _race = race;
+            _age = age;
+            _enOrdreDeVaccin = enOrdreDeVaccin;
+            _puce = puce;
+            _genre = genre;
+            _sterilise = sterilise;
+
+        }
+
+        public string AffichageChien()
+        {
+            string chaine = "Nom : " + _nom + "// Race : " + _race + "Age : " + _age + " // Vaccin : " + _enOrdreDeVaccin + " // Puce : " + _puce + " // Genre : " + _genre + " // Sterilisé : " + _sterilise + " // dresse : ";
+            return chaine;
+        }
+
+        public void Promener()
+        {
+            Console.WriteLine("Chien Promener");
+        }
+
+        public void Dresser()
+        {
+            Console.WriteLine("Chien Dresser");
+        }
+
+        public void Vacciner()
+        {
+            Console.WriteLine("Chien Vacciner");
+        }
+
+
+        public uint Age 
+        {
+            get
+            {
+                return _age;
+            }
+            set 
+            { 
+                if (value > 0)
+                {
+                    _age = value;
+                }
+            }
+        }
+    }
+}
---
+++ PholienVictor_ChienFinal/PholienVictor_Chien/Program.cs
@@ -0,0 +1,27 @@
+﻿using PholienVictor_Chien;
+
+namespace UAA14_Chien
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chien[] chiens = new Chien[10];
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Géneration des chiens");
+            for (uint i = 0; i < 10; i++)
+            {
+                Chien chien = new Chien("rex " + i, "TRex", 2 + i, false, false, "male", false, false);
+                chiens[i] = chien;
+                Console.WriteLine(chiens[i].AffichageChien());
+            }
+
+
+            for (uint i = 0; i < 10; i++)
+            {
+                Console.WriteLine(chiens[i].AffichageChien());
+            }
+
+        }
+    }
+}
